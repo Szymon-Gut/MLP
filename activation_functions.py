1dac5798afc674fa49f4c4761b3af1b721605a78
Szymon <72514490+Szymon-Gut@users.noreply.github.com>
2023-03-24 17:41:57 +0100

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -38,7 +38,7 @@ class Softmax(ActivationFunction):
             return np.exp(x) / (np.sum(np.exp(x), axis=1).reshape(
                 (x.shape[0], 1)) @ np.ones((1, x.shape[1])))
     
-    def derivative(self, x): # Best implementation (VERY FAST)
+    def derivative(self, x): 
         s = self.calculate(x).T
         a = np.eye(s.shape[-1])
         temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
