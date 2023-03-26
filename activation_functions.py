f84218b59cbb5a4ec9a79dba4be594cff1bbd4d9
Szymon-Gut <s.gucio@wp.pl>
2023-03-26 16:23:39 +0200

    NN5 activations added

--- activation_functions.py
+++ activation_functions.py
@@ -8,6 +8,46 @@ class ActivationFunction:
     def derivative(self, x):
         pass
 
+    def error(self, error, derivative):
+        pass
+
+
+class Relu(ActivationFunction):
+
+    def calculate(self, x):
+        return max(0, x)
+
+    def derivative(self, x):
+        return 0 if x < 0 else 1
+
+    def error(self, error, derivative):
+        return np.multiply(error, derivative)
+
+
+class LeakyRelu(ActivationFunction):
+    def calculate(self, x):
+        return max(0.1 * x, x)
+
+    def derivative(self, x, alpha=0.01):
+        return alpha if x < 0 else 1
+
+    def error(self, error, derivative):
+        return np.multiply(error, derivative)
+
+
+class Tanh(ActivationFunction):
+
+    def calculate(self, x):
+        counter = np.exp(x) - np.exp(-x)
+        denominator = np.exp(x) + np.exp(-x)
+        return counter / denominator
+
+    def derivative(self, x):
+        return 1 - np.square(self.calculate(x))
+
+    def error(self, error, derivative):
+        return np.multiply(error, derivative)
+
 
 class Sigmoid(ActivationFunction):
 
@@ -17,6 +57,9 @@ class Sigmoid(ActivationFunction):
     def derivative(self, x):
         return np.exp(-x) / np.square((1 + np.exp(-x)))
 
+    def error(self, error, derivative):
+        return np.multiply(error, derivative)
+
 
 class Linear(ActivationFunction):
 
@@ -25,10 +68,11 @@ class Linear(ActivationFunction):
 
     def derivative(self, x):
         return np.ones(shape=(x.shape[-1], 1))
-    
+
     def error(self, error, derivative):
         return np.multiply(error, derivative)
-    
+
+
 class Softmax(ActivationFunction):
 
     def calculate(self, x):
@@ -37,17 +81,16 @@ class Softmax(ActivationFunction):
         if x.shape[1] > 1:
             return np.exp(x) / (np.sum(np.exp(x), axis=1).reshape(
                 (x.shape[0], 1)) @ np.ones((1, x.shape[1])))
-    
-    def derivative(self, x): # Best implementation (VERY FAST)
+
+    def derivative(self, x):
         s = self.calculate(x).T
         a = np.eye(s.shape[-1])
-        temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-        temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-        temp1 = np.einsum('ij,jk->ijk',s,a)
-        temp2 = np.einsum('ij,ik->ijk',s,s)
-        return (temp1-temp2).T
-    
+        temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]), dtype=np.float32)
+        temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]), dtype=np.float32)
+        temp1 = np.einsum('ij,jk->ijk', s, a)
+        temp2 = np.einsum('ij,ik->ijk', s, s)
+        return (temp1 - temp2).T
+
     def error(self, error, derivative):
         derivative = derivative.reshape(derivative.shape[0], derivative.shape[1])
         return (derivative @ error).reshape((-1, 1))
-
